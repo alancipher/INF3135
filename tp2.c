224d192bfc756e527ba6dce2f5c570d0cf33425d
alancipher <alansame86@gmail.com>
2020-03-14 16:25:07 -0400

    dernier commit

--- tp2.c
+++ tp2.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+/*#include <stdlib.h>
 #include <stdio.h>
 #include <math.h>
 
@@ -6,7 +6,7 @@
 bool angle_incidence_marge_v1(float angle1, float angle2){
 	bool margeValid= false ;
  double marge = fabs(angle1) - fabs(angle2);
-   if (marge <= 0.25){
+   if (marge <= 0.250){
    	margeValid = true;
    }
 return margeValid;
@@ -28,36 +28,45 @@ return etatAppareil;
 
 bool tarmac_angle_conforme_v1(float angle){
 
-
-
+ return (angle <= 0.200 && angle1 >= -0.200);
 }
 
 
 bool decollage_angle_conforme_v2(float angle){
 
-
+ return (angle <= 51.000 && angle1 >= 2.500);
 }
 
 bool vol_angle_conforme_v3(float angle){
 
-
+return (angle <= 9.000 && angle1 >= -9.000);
 }
 
 bool atterrissage_angle_conforme_v1(float angle){
-
+return (angle <= -0.500 && angle1 >= -29.500);
 
 }
 
 
 bool volet_ouvert_v1(float longueur){
 
+return ( (longueur >= 0.900) && volet_longueur_max( longueur));
 
+}
 
+bool volet_longueur_max(float longueur){
+
+	return (longueur<= 108.000);
 }
 
 bool volet_ouverture_marge_v3(float longGauche, float longDroite){
 
-
+bool margeValid= false ;
+ double marge = fabs(longGauche) - fabs(longDroite);
+   if (marge <= 1.250){
+   	margeValid = true;
+   }
+return margeValid;
 
 }
 
@@ -73,7 +82,7 @@ int main (int argc, char* argv;){
 }
 
 
-
+*/
 
 
 

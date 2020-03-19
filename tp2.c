3f40d13869d943fa3767e33e6f267e7ebd7a01f0
alancipher <alansame86@gmail.com>
2020-03-18 22:54:51 -0400

    dernier commit

--- tp2.c
+++ tp2.c
@@ -240,7 +240,8 @@ int main(int argc, char *argv[]) {
     // gerer les trx 
        if (strcmp (trx, "01") == 0){
 
-       	char valeur[20] = *litrois;
+       	char valeur[20];
+       	 valeur = litrois;
 
        	if (!verifierEtat(litrois)){
           // gestion de valeur inacceptable pour etat avion 
